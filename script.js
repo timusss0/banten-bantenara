dbc11f1d10427c3a219a3fefd8e1aa479a46e199
Dhimas Nurhidayat <dhimasdn15@gmail.com>
2024-02-26 01:34:30 +0700

    update navbar

--- script.js
+++ script.js
@@ -11,61 +11,32 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// const navbarNav = document.querySelector('.navbar-nav');
-//  document.querySelector('#humberger').onclick=()=>{
-//       navbarNav.classList.toggle('active')
-//  };
+// navbar efek
+var lastScrollTop = 0;
+var navbar = document.getElementById("navbar");
 
-//  const humberger = document.querySelector('#humberger');
-//  document.addEventListener('click',function(e) {
-//      if(!humberger.contains(e.target)&& !navbarNav.contains(e.target)){
-//           navbarNav.classList.remove('active');
-//      }
-     
-//  });
+window.addEventListener("scroll", function () {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
+    // Atur transparansi navbar saat berada di paling atas atau sebelum di-scroll
+  
 
- //navbar efek
-//  var lastScrollTop = 0;
-//  var navbar = document.getElementById("navbar");
+    // Atur posisi top navbar saat scroll
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-80px"; // Sembunyikan navbar saat scroll ke bawah
+    } else {
+        navbar.style.top = "0"; // Tampilkan kembali navbar saat scroll ke atas
+    }
 
-//  window.addEventListener("scroll", function(){
-//      var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-
-//      // Atur transparansi navbar saat berada di paling atas atau sebelum di-scroll
-//      if (scrollTop <= 0) {
-//           navbar.classList.remove("active");
-//          navbar.style.color = "white !important";
-//      } else {
-//           navbar.classList.add("active");
-//           navbar.style.color = "black";
-//      }
-
-     // Atur posisi top navbar saat scroll
-//      if (scrollTop > lastScrollTop) {
-//          navbar.style.top = "-80px"; // Sembunyikan navbar saat scroll ke bawah
-//      } else {
-//          navbar.style.top = "0"; // Tampilkan kembali navbar saat scroll ke atas
-//      }
-
-//      lastScrollTop = scrollTop;
-//  });
-//  navbar.addEventListener("mouseenter", function() {
-//      navbar.classList.add("active");
-//      navbar.style.color = "black"; // Warna teks navbar saat di-hover
-//  });
+    lastScrollTop = scrollTop;
+});
 
 
- // Hapus kelas active saat hover dihilangkan
-//  navbar.addEventListener("mouseleave", function() {
-//      if (lastScrollTop <= 0) {
-//          navbar.classList.remove("active");
-//          navbar.style.color = "white"; // Warna teks navbar saat belum di-hover
-//      }
-//  });
+// end navbar efek
 
-//  container
+
+
+// container
 
   document.addEventListener("DOMContentLoaded", function () {
     var containerPanel = document.querySelector(".container-panel");
@@ -133,4 +104,4 @@ function showSlider(type){
         carouselDom.classList.remove('prev');
     });
 }
-
+// end carousel
